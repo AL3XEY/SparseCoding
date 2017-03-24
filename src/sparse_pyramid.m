5614882d33a368bc2596b83d0a524a038c328ebd
Alexandre Faucher <allemagne7@hotmail.fr>
2017-03-24 15:52:05 +0100

    Update sparse_pyramid

--- src/sparse_pyramid.m
+++ src/sparse_pyramid.m
@@ -11,27 +11,37 @@ addpath('../fast-sc-2/code/sc2/nrf/');
 
 %%%%% Parameters %%%%%
 
-load ../res/att_faces/facesScaled.mat
-images = faces(:,:,1:1)+0.5;
+n_levels = 3; % nombre de niveaux de la pyramide.
+
+%load ../res/att_faces/facesScaled.mat
+%images = faces(:,:,1:1)+0.5;
+load('../res/ZDB/zdbSmallScaled.mat');
+images = zdb(:,:,1:10)+0.5;
 [h w n_imgs] = size(images);
 
 %% sparse coding
-num_bases = 128;
-beta = 0.1;
-batch_size = 1000;
-num_iters = 1;
+for j=1:n_levels+1
+	num_bases{j} = 128;
+	beta{j} = 0.05; %0.01
+	betaReconstruct{j} = beta{j};
+	batch_size{j} = 1000;
+	num_iters{j} = 3;
+	fname_save{j} = [];
+	Binit{j} = [];
+end
 sparsity_func= 'L1';
 epsilon = [];
-fname_save = [];
-Binit = [];
 windowsize = 8;
+pars.display_images = false;
+pars.display_every = 0;
+pars.save_every = 0;
+pars.save_basis_timestamps = true;
 
 %%%%% Apply Laplacian pyramid algorithm to every image %%%%%
 
 hgf = [1,4,6,4,1]/16; % demi filtre binomial à 5 points
 [gfx,gfy] = meshgrid(hgf,hgf);
 Gausfilt = gfx.*gfy; % filtre binomial
-n_levels = 2; % nombre de niveaux de la pyramide.
 for i=1:n_imgs
 	[Lplac] = laplacianPyramidDecomposition(images(:,:,i), n_levels);
 	for j=1:n_levels+1
@@ -50,7 +60,8 @@ for j=1:n_levels+1
 	end
 	%X{j} = getdata_imagearray_all(arr, windowsize);
 	X{j} = getdata_imagearray(arr, windowsize, 1024);
-	[B{j} S{j} stat{j}] = sparse_coding(X{j}, num_bases, beta, sparsity_func, epsilon, num_iters, batch_size, fname_save, Binit);
+	%X{j} = getdata_imagearray(arr, windowsize, floor(1024/j/j/2));
+	[B{j} S{j} stat{j}] = sparse_coding(X{j}, num_bases{j}, beta{j}, sparsity_func, epsilon, num_iters{j}, batch_size{j}, fname_save{j}, Binit{j});
 end
 
 %%%%% Apply Laplacian pyramid algorithm to test image %%%%%
@@ -61,7 +72,7 @@ img = images(:,:,1);
 %%%%% For each scale, for each signal, reconstruct it using corresponding dictionnary %%%%%
 
 for j=1:n_levels+1
-	[I In Iout{j} Sout] = reconstruction(Lplac{j}, B{j}, beta, [], []);
+	[I In Iout{j} Sout] = reconstruction(Lplac{j}, B{j}, betaReconstruct{j}, [], []);
 end
 
 %%%%% Reconstruct test image using pyramid %%%%%
