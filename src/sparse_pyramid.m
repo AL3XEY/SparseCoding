e50191f6ccb817115b6858401f16d5cc37f667b1
Alexandre Faucher <allemagne7@hotmail.fr>
2017-03-28 17:52:39 +0200

    Add display, stats and dict initialization

--- src/sparse_pyramid.m
+++ src/sparse_pyramid.m
@@ -11,31 +11,36 @@ addpath('../fast-sc-2/code/sc2/nrf/');
 
 %%%%% Parameters %%%%%
 
-n_levels = 3; % nombre de niveaux de la pyramide.
+n_levels = 4; % nombre de niveaux de la pyramide.
 
 %load ../res/att_faces/facesScaled.mat
 %images = faces(:,:,1:1)+0.5;
 load('../res/ZDB/zdbSmallScaled.mat');
-images = zdb(:,:,1:10)+0.5;
+images = zdb(:,:,1:1)+0.5;
 [h w n_imgs] = size(images);
 
 %% sparse coding
+winsize = 8;
 for j=1:n_levels+1
 	num_bases{j} = 128;
-	beta{j} = 0.05; %0.01
+	beta{j} = 0.01; %0.05
 	betaReconstruct{j} = beta{j};
 	batch_size{j} = 1000;
 	num_iters{j} = 3;
 	fname_save{j} = [];
-	Binit{j} = [];
+	Bi = rand(winsize^2,num_bases{j})-0.5;
+	Bi = Bi - repmat(mean(Bi,1), size(Bi,1),1);
+    Bi = Bi*diag(1./sqrt(sum(Bi.*Bi)));
+	Bi(:,1) = ones(winsize^2,1)*(-0.126);
+	%Binit{j} = [];
+	Binit{j} = Bi;
 end
 sparsity_func= 'L1';
 epsilon = [];
-windowsize = 8;
-pars.display_images = false;
-pars.display_every = 0;
-pars.save_every = 0;
-pars.save_basis_timestamps = true;
+pars.display_images = true;%false;
+pars.display_every = 1;%0;
+pars.save_every = 1;%0;
+pars.save_basis_timestamps = false;%true;
 
 %%%%% Apply Laplacian pyramid algorithm to every image %%%%%
 
@@ -51,17 +56,16 @@ end
 
 %%%%% Run through each scale and learn a dictionnary %%%%%
 
-arr = zeros(h, w, n_imgs)
 for j=1:n_levels+1
 	[hh ww nn] = size(decompositions{j});
 	arr = zeros(hh, ww, nn);
 	for i=1:n_imgs
 		arr(:,:,i) = decompositions{j}(:,:,i);
 	end
-	%X{j} = getdata_imagearray_all(arr, windowsize);
-	X{j} = getdata_imagearray(arr, windowsize, 1024);
-	%X{j} = getdata_imagearray(arr, windowsize, floor(1024/j/j/2));
-	[B{j} S{j} stat{j}] = sparse_coding(X{j}, num_bases{j}, beta{j}, sparsity_func, epsilon, num_iters{j}, batch_size{j}, fname_save{j}, Binit{j});
+	X{j} = getdata_imagearray_all(arr, winsize);
+	%X{j} = getdata_imagearray(arr, winsize, 1024);
+	%X{j} = getdata_imagearray(arr, winsize, floor(1024/j/j/2));
+	[B{j} S{j} stat{j}] = sparse_coding(X{j}, num_bases{j}, beta{j}, sparsity_func, epsilon, num_iters{j}, batch_size{j}, fname_save{j}, pars, Binit{j});
 end
 
 %%%%% Apply Laplacian pyramid algorithm to test image %%%%%
@@ -72,21 +76,53 @@ img = images(:,:,1);
 %%%%% For each scale, for each signal, reconstruct it using corresponding dictionnary %%%%%
 
 for j=1:n_levels+1
-	[I In Iout{j} Sout] = reconstruction(Lplac{j}, B{j}, betaReconstruct{j}, [], []);
+	[I In Iout{j} Sout{j} entropyI{j} entropyInoised entropyIout{j} PSNR_In PSNR_Iout{j} fresidue{j} fsparsity{j} sparsity{j}] = reconstruction(Lplac{j}, B{j}, betaReconstruct{j}, [], []);
 end
 
 %%%%% Reconstruct test image using pyramid %%%%%
-
+Iout{n_levels+1} = Iout{n_levels+1}+0.5; %TODO why???
 imgout = laplacianPyramidReconstruction(Iout, n_levels);
 
 %%%%% Display %%%%%
 
+close all
+
+for j=1:n_levels+1
+	figure;
+	colormap gray;
+	imagesc(Lplac{j}, [0 1]);
+end
+
+for j=1:n_levels+1
+	figure;
+	colormap gray;
+	imagesc(Iout{j}, [0 1]);
+end
+
 img = img-0.5;
-imgout = imgout;%-0.5;
-close all;
+imgout = imgout-0.5;%-0.5;
 figure;
 colormap gray;
 imagesc(img, [-0.5 0.5]);
 figure;
 colormap gray;
 imagesc(imgout, [-0.5 0.5]);
+entropyImg = entropy(img)
+entropyImgout = entropy(imgout)
+if is_octave || ~verLessThan('matlab', '8.3') %if Matlab R2014a and above
+	PSNRImgout = psnr(imgout, img)
+else
+	PSNRImgout = -1;
+end
+
+meanPSNR = 0;
+meanSparsity = 0;
+avgnzero = 0;
+for j=1:n_levels+1
+	meanPSNR = meanPSNR + PSNR_Iout{j};
+	meanSparsity = meanSparsity + sparsity{j};
+	avgnzero = avgnzero + sparsity{j}*size(B{j},2);
+end
+meanPSNR = meanPSNR / j
+meanSparsity = meanSparsity / j
+avgnzero = avgnzero / j %not really pertinent if levels have different dict size
