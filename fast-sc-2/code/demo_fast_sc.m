edc2fc6f41ae5c6e9e3225b17c2323cdb752aabc
Alexandre Faucher <allemagne7@hotmail.fr>
2017-03-29 11:07:06 +0200

    Fix fast-sc-2 main (demo)

--- fast-sc-2/code/demo_fast_sc.m
+++ fast-sc-2/code/demo_fast_sc.m
@@ -19,8 +19,9 @@ end
 %X = getdata_imagearray_all(IMAGESr, 8);
 %X = getdata_imagearray_all(IMAGESr(:,:,4), 8);
 load ../../res/att_faces/facesScaled.mat
-%X = getdata_imagearray(faces, 8, 4096);
-X = getdata_imagearray_all(faces(:,:,1:20), 8);
+winsize = 8;
+X = getdata_imagearray(faces, winsize, 4096);
+%X = getdata_imagearray_all(faces(:,:,1:20), winsize);
 %load ../../res/ZDB/zdbSmallScaled.mat
 %X = getdata_imagearray(zdb, 8, 4096);
 
@@ -28,7 +29,7 @@ X = getdata_imagearray_all(faces(:,:,1:20), 8);
 num_bases = 128; %number of atoms in the dictionnary
 beta = 0.1; %Lagrange multiplier
 batch_size = 1000;
-num_iters = 3; %number of iterations of the learning algorithm
+num_iters = 10; %number of iterations of the learning algorithm
 if opt_choice==1
     sparsity_func= 'L1';
     epsilon = [];
@@ -38,8 +39,15 @@ elseif opt_choice==2
 end
 
 Binit = []; %Binit = getdata_imagearray(faces, 8, num_bases);
+%Bi = rand(winsize^2,num_bases)-0.5;
+%Bi = Bi - repmat(mean(Bi,1), size(Bi,1),1);
+%Bi = Bi*diag(1./sqrt(sum(Bi.*Bi)));
+%%Bi(:,1) = ones(winsize^2,1)*(-0.126);
+%load ../results/dict/19/19.mat
+%Bi(:,1) = B(:,129);
+%Binit = Bi;
 
 fname_save = sprintf('../results/sc_%s_b%d_beta%g_%s', sparsity_func, num_bases, beta, datestr(now, 30));
 
 % run fast sparse coding
-[B S stat] = sparse_coding(X, num_bases, beta, sparsity_func, epsilon, num_iters, batch_size, fname_save, Binit);
+[B S stat] = sparse_coding(X, num_bases, beta, sparsity_func, epsilon, num_iters, batch_size, fname_save, [], Binit);
