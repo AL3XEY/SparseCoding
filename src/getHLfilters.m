8badb9b301ba81290a522eb26797f3b084e070dc
Alexandre Faucher <allemagne7@hotmail.fr>
2017-07-11 11:27:14 +0200

    Fix getHLfilters with global filter count parameter
    We don't give as parameter the number of filters (patches) to take per
    image but the global number we want, regardless of the size of the
    dataset.

--- src/getHLfilters.m
+++ src/getHLfilters.m
@@ -1,9 +1,6 @@
 function [HLfilters] = getHLfilters(imgs, nHL, display)
     if nargin<2 || isempty(nHL)
-        nHL = 10;
-        %nHL = 2; %the number of prototypes to take per image
-        %nHL = 100;
-        %nHL = 1000;
+        nHL = 1000; %the number of prototypes to take (total) across all images
     end
     if nargin<3 || isempty(display)
         display = false;
@@ -12,19 +9,49 @@ function [HLfilters] = getHLfilters(imgs, nHL, display)
     if is_octave
 		pkg load image;
     end
+
+    %Adapt the number of images to the number of HL filters wanted
+    %TODO : this was easy to code but not optimal because if nHL >> nimg,
+    %we store a big number of images (we could store nimg images and access
+    %them when multiple times instead)
+    %+ the random selection might have non-unique values (it's a very small
+    %risk, but still, we generate new x and y every new image)
+    %tl;dr : this solution is fast but costs a lot of memory
+    nimg = size(imgs,2);
+    images = cell(1,nHL);
+    if nHL < nimg
+		perms = randperm(nimg,nHL);
+		for i=1:size(perms,2)
+			images{i}=imgs{perms(i)};
+		end
+		imgs = images;
+    end
+    if nHL > nimg
+		iterations = round(nHL/nimg);
+		additions = mod(nHL,nimg);
+        for i=1:nimg*iterations
+            images{i} = imgs{mod(i-1,nimg)+1};
+        end
+        if additions
+            perms = randperm(nimg,additions);
+            for i=1:additions
+                images{nimg*iterations+i}=imgs{perms(i)};
+            end
+        end
+		imgs=images;
+    end
+    nimg = size(imgs,2);
+    clear images
     
-	HMAXparams = HMAXparameters();
-
-	nimg = size(imgs,2);
-
+    HMAXparams = HMAXparameters();
     HLfilters = cell(1,HMAXparams.nscales-1);
 	for scal=1:HMAXparams.nscales-1
 		sz = HMAXparams.filter_sz(scal);
-		HLfilters{scal} = zeros(sz, sz, HMAXparams.nth, nHL*nimg);
+		HLfilters{scal} = zeros(sz, sz, HMAXparams.nth, nimg);
 	end
 
 	gaborFilters = getGaborFilters(HMAXparams, display); % build Gabor filters
-
+    
 	for imgcpt=1:nimg
 		img = imgs{imgcpt};
 		if size(img,3)==3
@@ -35,31 +62,29 @@ function [HLfilters] = getHLfilters(imgs, nHL, display)
             figure
             imshow(uint8(255*img)) % show original image
         end
-        
+
 		S1 = getS1(img, gaborFilters, HMAXparams, display);
 
 		C1 = getC1(S1, dx, dy, HMAXparams, display);
-		
+
         %Take random patches from C1
-        rdx = 1+floor(rand(nHL,1).*(dx)); %TODO unique ?
-        rdy = 1+floor(rand(nHL,1).*(dy));
+        rdx = 1+floor(rand().*(dx)); %TODO unicity ?
+        rdy = 1+floor(rand().*(dy));
         for scal=1:HMAXparams.nscales-1
 			sz = HMAXparams.filter_sz(scal);
 			[dx2,dy2,~]=size(C1{scal});
             maxX = dx2-sz; %TODO if maxX<0, stop
             maxY = dy2-sz; %TODO if maxY<0, stop
             ratio = dx2/dx;
-			for cpt=1:nHL
-                x = round(rdx(cpt)*ratio); %TODO calculate it as vector, before this loop ?
-                y = round(rdy(cpt)*ratio);
-                if x>maxX
-                    x = maxX;
-                end
-                if y>maxY
-                    y = maxY;
-                end
-				HLfilters{scal}(1:sz,1:sz,:,(imgcpt-1)*nHL+cpt) = C1{scal}(x:x+sz-1,y:y+sz-1,:);
-			end
-		end
+            x = 1+round(rdx*ratio);
+            y = 1+round(rdy*ratio);
+            if x>maxX
+                x = maxX;
+            end
+            if y>maxY
+                y = maxY;
+            end
+            HLfilters{scal}(1:sz,1:sz,:,imgcpt) = C1{scal}(x:x+sz-1,y:y+sz-1,:);
+        end
 	end
 end
