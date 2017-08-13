b3f7ff7b838f9e3fb9626adcfb96a2990bf6bf5c
Alexandre Faucher <allemagne7@hotmail.fr>
2017-08-13 22:27:11 +0200

    Fix addSolidColor function

--- src/fast-sc-2/code/addSolidColor.m
+++ src/fast-sc-2/code/addSolidColor.m
@@ -1,12 +1,10 @@
-[B,pars] = function addSolidColor(B,pars)
+function [B,pars] = addSolidColor(B,pars)
     [h,w]=size(B);
-    Btmp = B;
-    if pars.channels==1
-		
-    end
-	if pars.channels==3
-
+    Btmp = zeros(h,w+pars.channels);
+    Btmp(1:h,1:w) = B;
+    for chan=1:pars.channels
+        Btmp(:,w+chan) = 0.25.*ones(h,1);
     end
     B = Btmp;
-    %TODO pars.num_bases = size(B,2);
+    pars.num_bases = size(B,2);
 end
