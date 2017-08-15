6498f09d8fb07beee2f31f6c85a72772ae0ee2a7
Alexandre Faucher <allemagne7@hotmail.fr>
2017-08-15 15:45:16 +0200

    Code fusion for getdata_imagearray function (patching function)

--- src/fast-sc-2/code/getdata_imagearray.m
+++ src/fast-sc-2/code/getdata_imagearray.m
@@ -1,34 +1,54 @@
-function X = getdata_imagearray(IMAGES, winsize, num_patches)
-
-[h,w,channels,num_images]=size(IMAGES)
-sz= winsize;
-BUFF=4;
-
-totalsamples = 0;
-% extract subimages at random from this image to make data vector X
-% Step through the images
-X= zeros((sz^2)*channels, num_patches);
-for i=1:num_images,
-
-    % Display progress
-    fprintf('[%d/%d]',i,num_images);
-
-    this_image=IMAGES(:,:,:,i);
-
-    % Determine how many patches to take
-    getsample = floor(num_patches/num_images);
-    if i==num_images, getsample = num_patches-totalsamples; end
-
-    % Extract patches at random from this image to make data vector X
-    for j=1:getsample
-        r=BUFF+ceil((h-sz-2*BUFF)*rand);
-        c=BUFF+ceil((w-sz-2*BUFF)*rand);
-        totalsamples = totalsamples + 1;
-        % X(:,totalsamples)=reshape(this_image(r:r+sz-1,c:c+sz-1),sz^2,1);
-		for chan=1:channels
-			temp = reshape(this_image(r:r+sz-1,c:c+sz-1,chan),sz^2,1);
-        	X((chan-1)*sz^2+1:chan*sz^2,totalsamples) = temp - mean(temp);
-		end
+function X = getdata_imagearray(IMAGES, winsize, type, option)
+    [h,w,channels,num_images]=size(IMAGES);
+    BUFF=4;
+    if strcmp(type,'rand')
+        num_patches = option;
+    else
+        if strcmp(type,'all')
+            foo = h - winsize + 1;
+            bar = w - winsize + 1;
+        end
+        if strcmp(type,'some')
+            foo = floor(h / winsize);
+            bar = floor(w / winsize);
+        end
+        patches_per_image = foo*bar;
+        num_patches = num_images * patches_per_image;
     end
+    cpt=1;
+    % extract subimages at random from this image to make data vector X
+    % Step through the images
+    X= zeros((winsize^2)*channels, num_patches);
+    for i=1:num_images,
+        % Display progress
+        fprintf('[%d/%d]',i,num_images);
+        this_image=IMAGES(:,:,:,i);
+        if strcmp(type,'rand')
+           % Determine how many patches to take
+            getsample = floor(num_patches/num_images);
+            if i==num_images
+                getsample = num_patches-cpt;
+            end
+            % Extract patches at random from this image to make data vector X
+            for j=1:getsample
+                r=BUFF+ceil((h-winsize-2*BUFF)*rand);
+                c=BUFF+ceil((w-winsize-2*BUFF)*rand);
+                cpt = cpt + 1;
+                for chan=1:channels
+                    temp = reshape(this_image(r:r+winsize-1,c:c+winsize-1,chan),winsize^2,1);
+                    X((chan-1)*winsize^2+1:chan*winsize^2,cpt) = temp - mean(temp);
+                end
+            end 
+        else
+            for j=1:foo
+                for k=1:bar
+                    for chan=1:channels
+                        X((chan-1)*winsize^2+1:chan*winsize^2,cpt) = reshape(this_image(j:j+winsize-1, k:k+winsize-1, chan),winsize^2,1)';
+                    end
+                    cpt = cpt+1;
+                end
+            end
+        end
+    end
+    fprintf('\n');
 end
-fprintf('\n');
